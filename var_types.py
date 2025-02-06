4662b052adb700ea43855a10d2d4f64c4d651384
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2025-02-06 09:38:57 -0600

    lists added

--- var_types.py
+++ var_types.py
@@ -14,3 +14,15 @@ new_string = basic + ' I am here' #Hello, World! I am here
 
 #list basics
 fruits = ["apple", 20, "$20"] #lists can contain multiple data types
+print(fruits[0]) #apple
+fruits[1] = "banana" #lists are changeable
+print(fruits) #['apple', 'banana', '$20']
+fruits.append("orange") #add an item to the end of the list
+print(fruits) #['apple', 'banana', '$20', 'orange']
+fruits.remove("$20") #remove an item from the list, input is a value in the list
+print(fruits) #['apple', 'banana', 'orange']
+fruits.insert(2, "mango") #add an item at a specific index
+print(fruits) #['apple', 'banana', 'mango', 'orange']
+fruits.pop() #remove the last item from the list or a specific index
+print(fruits) #['apple', 'banana', 'mango']
+print(fruits.index("banana")) #1, returns the index of the first occurrence of the value
