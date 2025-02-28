494e7664f05b3d725204245eba2bdaf0986222c4
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2025-02-28 13:56:18 -0600

    branching pt2

--- branching.py
+++ branching.py
@@ -14,3 +14,57 @@ if x!=y: # != means not equal to
     print('this is extra') 
 else:    
     print('x is equal to y')       # this line is not executed
+
+ 
+#relational operators are used to compare values
+# <, >, <=, >=, ==, !=  
+
+#detecting ranges with branches 
+num_outfits = int(input())
+
+if num_outfits > 11:
+    print('Must pack less')
+elif 5 <= num_outfits <= 11:
+    print('Larger suitcase')
+elif 0 <= num_outfits <= 4:
+    print('Medium suitcase')
+elif num_outfits <= 0:
+    print('Bad input')
+
+#logical operators are used to combine conditions
+# and, or, not
+if num_outfits > 11:
+    print('Must pack less')
+elif (5 <= num_outfits) and (num_outfits <= 11):
+    print('Larger suitcase')
+elif (0 <= num_outfits) and (num_outfits <= 4):
+    print('Medium suitcase')
+elif (num_outfits < 0) or (num_outfits == 0):
+    print('Bad input')
+
+#membership operators
+# in, not in
+prices = [10, 20, 30, 40, 50]
+if 20 in prices:
+    print('20 is in prices')
+if 100 not in prices:
+    print('100 is not in prices')
+
+#identity operators
+# is, is not
+x = 10
+y = 10
+if x is y:
+    print('x and y are the same')
+if x is not 20:
+    print('x is not 20')    
+
+animal = input()
+adjective1 = input()
+
+# Modify the following line
+animals_string = ("Lane's favorite exhibit to look at when she goes to the zoo is "
+                    "the {0} exhibit. She thinks the {0}s are {1} and cool.") #breaks the string up
+# format() replaces the curly braces in a string with variables.
+new_str = animals_string.format(animal, adjective1)
+print(new_str)
